09f098d29c9c7506c478ce3c98a3fcd14d229689
samkong <ss.samkong@gmail.com>
2026-09-11 00:23:10 +0800

    Privacy and Terms pages on the homepage template: green hero, readable sections with headings, on-page contents, contact card; footer names Moka Venture Sdn Bhd

--- resources/views/partials/site-footer.blade.php
+++ resources/views/partials/site-footer.blade.php
@@ -97,7 +97,7 @@
         <div class="mk-bottom">
             <div class="mk-brand">
                 <a href="/" aria-label="MOKA home"><img src="{{ asset('new-theme23/images/logo.png') }}" alt="MOKA" loading="lazy"></a>
-                <p>Malaysia's &#35;1 property management company.<br>{{ date('Y') }} © Owned and operated by Moka. All rights reserved.</p>
+                <p>Malaysia's &#35;1 property management company.<br>{{ date('Y') }} © Owned and operated by Moka Venture Sdn Bhd. All rights reserved.</p>
             </div>
             <form class="mk-news" method="POST" action="/subscribe" aria-label="Newsletter">
                 @csrf
